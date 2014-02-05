f65dc48f57fa4dd59dc1cf9967cc59ad8bcc82c2
Chris Carrington <carrington.christopher@gmail.com>
2014-02-05 13:54:05 -0500

    Added new home page image

---
+++ app/page-login/modal-forgot-details/forgot-details.js
@@ -0,0 +1,35 @@
+(function()
+{
+	var FogotDetailsModal = (function(){});
+	/*
+	var FogotDetailsModal = (function()
+	{
+		var self;
+		var Common = Import('Common');
+
+		var ForgotDetailsModal = function()
+		{
+			self = this;
+			self.init();
+		};
+
+		ForgotDetailsModal.prototype = 
+		{
+			init : function()
+			{
+				Common.setCurrentModal(self, 'ForgotDetailsModal', '#forgot-details');
+				Common.paintPage();
+			},
+			destroy : function(modal2modal)
+			{
+				Common.destroyModal(self, 'ForgotDetailsModal', '#forgot-details', modal2modal);
+			}
+		};
+
+		return ForgotDetailsModal;
+	})();
+
+	Export('ForgotDetailsModal', ForgotDetailsModal);
+	*/
+})();
+
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1352,6 +1352,41 @@ $(document).ready(function()
 	Export('DashboardPage', DashboardPage);
 })();(function()
 {
+	var FogotDetailsModal = (function(){});
+	/*
+	var FogotDetailsModal = (function()
+	{
+		var self;
+		var Common = Import('Common');
+
+		var ForgotDetailsModal = function()
+		{
+			self = this;
+			self.init();
+		};
+
+		ForgotDetailsModal.prototype = 
+		{
+			init : function()
+			{
+				Common.setCurrentModal(self, 'ForgotDetailsModal', '#forgot-details');
+				Common.paintPage();
+			},
+			destroy : function(modal2modal)
+			{
+				Common.destroyModal(self, 'ForgotDetailsModal', '#forgot-details', modal2modal);
+			}
+		};
+
+		return ForgotDetailsModal;
+	})();
+
+	Export('ForgotDetailsModal', ForgotDetailsModal);
+	*/
+})();
+
+(function()
+{
 	var LoginPage = (function()
 	{
 		var self;
